ecd8733f8f3921cd978651a7774176b544a8bdd2
Brant Watrous <watroubd@gmail.com>
2015-09-11 08:17:35 -0500

    Fix FlurryAnalytics object

--- www/flurryAnalytics.js
+++ www/flurryAnalytics.js
@@ -1,149 +1,152 @@
 
-function FlurryAnalytics() {
+var cordova = require('cordova'),
+    argscheck = require('cordova/argscheck'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec');
 
-	var featureName = featureName;
+var FlurryAnalytics = {};
+var featureName = 'FlurryAnalyticsPlugin';
 
-	/*
-	 the only function that is actually required to start tracking sessions
+/*
+ the only function that is actually required to start tracking sessions
 
-	 options:
+ options:
 
-	 version
-	 continueSessionSeconds       (must be less than or equal to five for Android)
-	 userId
-	 gender
-	 age
-	 logLevel                    (VERBOSE, DEBUG, INFO, WARN, ERROR)
-	 enableLogging               (defaults to false)
-	 enableEventLogging          (defaults to true)
-	 enableCrashReporting        (defaults to false, iOS only)
-	 enableBackgroundSessions    (defaults to false, iOS only)
-	 reportSessionsOnClose       (defaults to true, iOS only)
-	 reportSessionsOnPause       (defaults to true, iOS only)
-	 */
-	this.init = function(appKey /* [options], successCallback, failureCallback */) {
+ version
+ continueSessionSeconds       (must be less than or equal to five for Android)
+ userId
+ gender
+ age
+ logLevel                    (VERBOSE, DEBUG, INFO, WARN, ERROR)
+ enableLogging               (defaults to false)
+ enableEventLogging          (defaults to true)
+ enableCrashReporting        (defaults to false, iOS only)
+ enableBackgroundSessions    (defaults to false, iOS only)
+ reportSessionsOnClose       (defaults to true, iOS only)
+ reportSessionsOnPause       (defaults to true, iOS only)
+ */
+FlurryAnalytics.init = function(appKey /* [options], successCallback, failureCallback */) {
 
-		var successCallback,
-			failureCallback,
-			options;
+    var successCallback,
+        failureCallback,
+        options;
 
-		if(arguments.length === 4) {
-			options = arguments[1];
-			successCallback = arguments[2];
-			failureCallback = arguments[3];
-		} else if(arguments.length === 3) {
-			successCallback = arguments[1];
-			failureCallback = arguments[2];
-		} else if(arguments.length === 2) {
-			options = arguments[1];
-		}
+    if(arguments.length === 4) {
+        options = arguments[1];
+        successCallback = arguments[2];
+        failureCallback = arguments[3];
+    } else if(arguments.length === 3) {
+        successCallback = arguments[1];
+        failureCallback = arguments[2];
+    } else if(arguments.length === 2) {
+        options = arguments[1];
+    }
 
-		return cordova.exec(successCallback, failureCallback, featureName, 'initialize', [appKey, options]);
-	};
+    return cordova.exec(successCallback, failureCallback, featureName, 'initialize', [appKey, options]);
+};
 
-	// the params parameter is optional
-	this.logEvent = function(event /* [params], successCallback, failureCallback */) {
+// the params parameter is optional
+FlurryAnalytics.logEvent = function(event /* [params], successCallback, failureCallback */) {
 
-		var successCallback,
-			failureCallback,
-			params;
+    var successCallback,
+        failureCallback,
+        params;
 
-		if(arguments.length === 4) {
-			params = arguments[1];
-			successCallback = arguments[2];
-			failureCallback = arguments[3];
-		} else if(arguments.length === 3) {
-			successCallback = arguments[1];
-			failureCallback = arguments[2];
-		} else if(arguments.length === 2) {
-			params = arguments[1];
-		}
+    if(arguments.length === 4) {
+        params = arguments[1];
+        successCallback = arguments[2];
+        failureCallback = arguments[3];
+    } else if(arguments.length === 3) {
+        successCallback = arguments[1];
+        failureCallback = arguments[2];
+    } else if(arguments.length === 2) {
+        params = arguments[1];
+    }
 
-		return cordova.exec(successCallback, failureCallback, featureName, 'logEvent', [
-			event,
-			false,
-			params
-		]);
-	};
+    return cordova.exec(successCallback, failureCallback, featureName, 'logEvent', [
+        event,
+        false,
+        params
+    ]);
+};
 
-	// the params parameter is optional
-	this.startTimedEvent = function(event /* [params], successCallback, failureCallback */) {
+// the params parameter is optional
+FlurryAnalytics.startTimedEvent = function(event /* [params], successCallback, failureCallback */) {
 
-		var successCallback,
-			failureCallback,
-			params;
+    var successCallback,
+        failureCallback,
+        params;
 
-		if(arguments.length === 4) {
-			params = arguments[1];
-			successCallback = arguments[2];
-			failureCallback = arguments[3];
-		} else if(arguments.length === 3) {
-			successCallback = arguments[1];
-			failureCallback = arguments[2];
-		} else if(arguments.length === 2) {
-			params = arguments[1];
-		}
+    if(arguments.length === 4) {
+        params = arguments[1];
+        successCallback = arguments[2];
+        failureCallback = arguments[3];
+    } else if(arguments.length === 3) {
+        successCallback = arguments[1];
+        failureCallback = arguments[2];
+    } else if(arguments.length === 2) {
+        params = arguments[1];
+    }
 
-		return cordova.exec(successCallback, failureCallback, featureName, 'logEvent', [
-			event,
-			true,
-			params
-		]);
-	};
+    return cordova.exec(successCallback, failureCallback, featureName, 'logEvent', [
+        event,
+        true,
+        params
+    ]);
+};
 
-	// the params parameter is optional
-	this.endTimedEvent = function(event /* [params], successCallback, failureCallback */) {
+// the params parameter is optional
+FlurryAnalytics.endTimedEvent = function(event /* [params], successCallback, failureCallback */) {
 
-		var successCallback,
-			failureCallback,
-			params;
+    var successCallback,
+        failureCallback,
+        params;
 
-		if(arguments.length === 4) {
-			params = arguments[1];
-			successCallback = arguments[2];
-			failureCallback = arguments[3];
-		} else if(arguments.length === 3) {
-			successCallback = arguments[1];
-			failureCallback = arguments[2];
-		} else if(arguments.length === 2) {
-			params = arguments[1];
-		}
-		return cordova.exec(successCallback, failureCallback, featureName, 'endTimedEvent', [
-			event,
-			params
-		]);
-	};
+    if(arguments.length === 4) {
+        params = arguments[1];
+        successCallback = arguments[2];
+        failureCallback = arguments[3];
+    } else if(arguments.length === 3) {
+        successCallback = arguments[1];
+        failureCallback = arguments[2];
+    } else if(arguments.length === 2) {
+        params = arguments[1];
+    }
 
-	this.logPageView = function(successCallback, failureCallback) {
-		return cordova.exec(successCallback, failureCallback, featureName, 'logPageView', []);
-	};
+    return cordova.exec(successCallback, failureCallback, featureName, 'endTimedEvent', [
+        event,
+        params
+    ]);
+};
 
-	this.logError = function(code, message, successCallback, failureCallback) {
-		return cordova.exec(successCallback, failureCallback, featureName, 'logError', [code, message]);
-	};
+FlurryAnalytics.logPageView = function(successCallback, failureCallback) {
+    return cordova.exec(successCallback, failureCallback, featureName, 'logPageView', []);
+};
 
-	this.setLocation = function(location, message, successCallback, failureCallback) {
-		return cordova.exec(successCallback, failureCallback, featureName, 'setLocation', [
-			location.latitude,
-			location.longitude,
-			location.verticalAccuracy,
-			location.horizontalAccuracy
-		]);
-	};
+FlurryAnalytics.logError = function(code, message, successCallback, failureCallback) {
+    return cordova.exec(successCallback, failureCallback, featureName, 'logError', [code, message]);
+};
 
-	// only needed for older versions of Android
-	this.startSession = function(successCallback, failureCallback) {
-		return cordova.exec(successCallback, failureCallback, featureName, 'startSession', []);
-	};
+FlurryAnalytics.setLocation = function(location, message, successCallback, failureCallback) {
+    return cordova.exec(successCallback, failureCallback, featureName, 'setLocation', [
+        location.latitude,
+        location.longitude,
+        location.verticalAccuracy,
+        location.horizontalAccuracy
+    ]);
+};
 
-	// only needed for older versions of Android
-	this.endSession = function(successCallback, failureCallback) {
-		return cordova.exec(successCallback, failureCallback, featureName, 'endSession', []);
-	};
+// only needed for older versions of Android
+FlurryAnalytics.startSession = function(successCallback, failureCallback) {
+    return cordova.exec(successCallback, failureCallback, featureName, 'startSession', []);
+};
 
-}
+// only needed for older versions of Android
+FlurryAnalytics.endSession = function(successCallback, failureCallback) {
+    return cordova.exec(successCallback, failureCallback, featureName, 'endSession', []);
+};
+
 
 if(typeof module !== undefined && module.exports) {
-	module.exports = FlurryAnalytics;
+    module.exports = FlurryAnalytics;
 }
-
